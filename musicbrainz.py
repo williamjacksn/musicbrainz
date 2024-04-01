a91fe993cd7b7233e5b0aeb8fbad14ecff9cca31
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-04-01 09:41:46 -0500

    Remove known artists

--- musicbrainz.py
+++ musicbrainz.py
@@ -29,13 +29,3 @@ def get_session(username: str, password: str) -> requests.Session:
     resp = s.post(login_url, data=login_payload)
     resp.raise_for_status()
     return s
-
-
-known_artists = types.SimpleNamespace()
-known_artists.chris_tucci = types.SimpleNamespace(name='Chris Tucci', artist_id=1878272)
-known_artists.dave_edson = types.SimpleNamespace(name='Dave Edson', artist_id=2625984)
-known_artists.feodor_chin = types.SimpleNamespace(name='Feodor Chin', artist_id=2027455)
-known_artists.jessica_rau = types.SimpleNamespace(name='Jessica Rau', artist_id=1874204)
-known_artists.joshua_johnson = types.SimpleNamespace(name='Joshua Johnson', artist_id=1875608)
-known_artists.kym_miller = types.SimpleNamespace(name='Kym Miller', artist_id=2625985)
-known_artists.marion_toro = types.SimpleNamespace(name='Marion Toro', artist_id=2362948)
